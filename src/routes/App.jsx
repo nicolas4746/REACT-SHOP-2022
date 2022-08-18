4a870822468b62a634a357991738f0afb39f5a40
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-18 09:15:25 -0300

    navegacion con Router lista

---
+++ src/routes/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/global.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    	<Layout>
+        	<Routes>
+         		 <Route path="/" element={<Home />} />
+          		<Route path="/login" element={<Login />} />
+         		 <Route path="/recovery-password" element={<RecoveryPassword />} />
+          		<Route path="*" element={<NotFound />} />
+        	</Routes>
+      	</Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
