5394615d1677412567d5ccaa85cf626100fba50b
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-19 08:32:45 -0300

    sumando el total del carrito

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import OrderItem from '@components/OrderItem';
-import AppContext from '@context/AppContext';
+import AppContext from '../context/AppContext';
 import '@styles/MyOrder.scss';
 import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
 	const { state } = useContext(AppContext);
+
+	const sumTotal = () => {
+		const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	}
+
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -20,7 +27,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
