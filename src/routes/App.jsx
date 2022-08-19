2d41f0fd35f8d86408bec9c04fa773a4dcdbc65e
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-19 14:39:59 -0300

    Corrigiendo commits fallados de deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter basename='/REACT-SHOP-2022'>
+			<BrowserRouter>
 				<Layout>
 					<Routes>
 						<Route exact path="/" element={<Home/>} />
