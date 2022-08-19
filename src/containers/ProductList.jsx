154dcd51b19521f86f354551ed5711332e55a253
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-19 15:13:02 -0300

    solucionando httpS

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -4,9 +4,6 @@ import '@styles/ProductList.scss';
 import useGetProducts from '@hooks/useGetProducts';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
-
-
-
 const ProductList = () => {
 
 	const products = useGetProducts(API);
